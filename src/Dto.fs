71e2af09216e5cbcc3b2824c3710b883fe52c938
Petr Chromec <petr.chromec@lmc.eu>
2020-01-27 15:05:02 +0100

    Add basic Dto

---
+++ src/Dto.fs
@@ -0,0 +1,39 @@
+namespace Lmc.Neo4j
+
+//
+// DTOs - must be public
+//
+module Dto =
+    open Lmc.Neo4j
+
+    // Common
+
+    type Name () =
+        let mutable nameValue = ""
+
+        member __.Name
+            with get() = nameValue
+            and set (value) = nameValue <- value
+
+    type NameAndLabel () =
+        let mutable nameValue = ""
+        let mutable labelValue = ""
+
+        member __.Name
+            with get() = nameValue
+            and set (value) = nameValue <- value
+
+        member __.Label
+            with get() = labelValue
+            and set (value) = labelValue <- value
+
+    let withNameAndLabel name label =
+        NameAndLabel (
+            Name = (name |> Name.value),
+            Label = (label |> Label.value)
+        )
+
+    let withName name =
+        Name (
+            Name = (name |> Name.value)
+        )
